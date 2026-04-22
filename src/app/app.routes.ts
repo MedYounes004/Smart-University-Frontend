ba3230c41bc72c5debab298e0aed168c484c7a51
youssef <youssef.kadob@gmail.com>
2026-04-22 14:23:15 +0100

    Fix unresolved merge conflict in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,30 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-<<<<<<< HEAD
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: 'courses',
-    loadComponent: () => import('./pages/Courses/courses.page').then((m) => m.CoursesPage),
-  },
-  {
-=======
->>>>>>> fe5ab535c2a5413bd54c1be322f1cdd9844cf4a7
     path: '',
     redirectTo: 'ai-assisstant',
     pathMatch: 'full',
   },
   {
     path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'courses',
     loadComponent: () =>
-      import('./home/home.page').then((m) => m.HomePage),
+      import('./pages/Courses/courses.page').then((m) => m.CoursesPage),
   },
   {
     path: 'ai-assisstant',
-    loadComponent: () => import('./pages/ai-assisstant/ai-assisstant.page').then( m => m.AiAssisstantPage)
+    loadComponent: () =>
+      import('./pages/ai-assisstant/ai-assisstant.page').then(
+        (m) => m.AiAssisstantPage,
+      ),
   },
-
- 
 ];
